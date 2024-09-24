55bfafde16c69582cd62359247fa1b4c7e7dc9be
David Bellaiche <b.david91@gmail.com>
2024-09-24 17:33:51 +0200

    add popularity score

---
+++ prov_rescale_popularity_score.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from slugify import slugify
+
+csv_file = 'paris_activities_high_touristic.csv'
+df = pd.read_csv(csv_file)
+df["slug"] = df["name"].apply(lambda x: slugify(x))
+df = df.drop(columns=df.filter(like='popularity_score').columns)
+
+pop_score_file = 'popularity_score.csv'
+df_pop_score = pd.read_csv(pop_score_file)
+df_pop_score["popularity_score"] = round(9 * ((df_pop_score['popularity_score'] - df_pop_score['popularity_score'].min()) / (df_pop_score['popularity_score'].max() - df_pop_score['popularity_score'].min())) + 1 )
+
+new_df = df.merge(df_pop_score, how="left", on="slug")
+print(new_df.head(5))
+
+new_df.to_csv('paris_activities_high_touristic.csv', index=False, encoding='utf-8-sig')
